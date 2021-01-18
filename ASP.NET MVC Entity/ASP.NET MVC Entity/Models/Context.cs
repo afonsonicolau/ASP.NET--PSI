f6d068933f47c8b500304a0a5fb23f13960a2f5f
Afonso <afonso9980@gmail.com>
2021-01-18 16:01:46 +0000

    ASP.NET - Final

---
+++ ASP.NET MVC Entity/ASP.NET MVC Entity/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ASP.NET_MVC_Entity.Models
+{
+    public class Context:DbContext
+    {
+        public DbSet<Person> people { get; set; }
+    }
+}
---
+++ ASP.NET MVC Entity/ASP.NET MVC Entity/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Entity.Models
+{
+    public class Person
+    {
+        [Key]
+        public int iCode { get; set; }
+        public string sName { get; set; }
+        public string sEmail { get; set; }
+    }
+}
